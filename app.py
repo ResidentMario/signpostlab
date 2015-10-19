19d57309d68ca90fa3e8c45d27793bf5db71bf0c
ResidentMario <aleksey.bilogur@gmail.com>
2015-10-19 12:49:20 -0400

    Still testing...

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! Got this server working---and Git too!'
+    return 'Hello World! Got this server working---and Git too! (2)'
 
 if __name__ == '__main__':
     app.run()
