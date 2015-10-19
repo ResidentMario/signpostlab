c9355492bfe9d882c5b86d7dced366c6aa365ade
ResidentMario <aleksey.bilogur@gmail.com>
2015-10-18 23:23:39 -0400

    Testing connection between Labs and git...

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! Got this server working---and Git too!'
 
 if __name__ == '__main__':
     app.run()
-    
