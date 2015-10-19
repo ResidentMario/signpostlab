142e69657ae9cb1cbf8f14930d810d768fa1f0ee
ResidentMario <aleksey.bilogur@gmail.com>
2015-10-19 13:04:41 -0400

    Creating some files, moving slowly to keep things working...

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! Got this server working---and Git too! (2)'
+def index():
+    # return 'Hello World! Got this server working---and Git too! (2)'
+    return render_template('frame.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
