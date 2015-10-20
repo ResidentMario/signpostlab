fee5670c906fd0f703fa66b0770b3f2388fd87f6
ResidentMario <aleksey.bilogur@gmail.com>
2015-10-19 22:18:50 -0400

    Build out a bunch of stuff.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import blogimporter
+import tnimporter
 
 app = Flask(__name__)
 
@@ -22,20 +24,19 @@ def blog_importer():
 		if not data or 'blog.wikimedia.org' not in data:
 			return render_template('blog_importer.html', code_returned="Invalid input. Make sure that you inputted the URL of a valid WM Blog post.")
 		else:
-			# return render_template('blog_importer.html', code_returned=os.system("Blog_Importer.py " + data))
-			# return render_template('blog_importer.html', code_returned=subprocess.check_output(["ls", '-l'], universal_newlines=True))
-			# return render_template('blog_importer.html', code_returned=Blog_Importer.import_blog(data, ''))
-			pass
+			return render_template('blog_importer.html', code_returned=blogimporter.main(data))
+			# return blogimporter.main(data)
 	else:
-		return render_template('blog_importer.html')
+			return render_template('blog_importer.html')
 
 @app.route('/tn_importer', methods=['GET', 'POST'])
 def tn_importer():
 	if request.method == 'POST':
-		pass
+		return render_template('tn_importer.html', code_returned=tnimporter.main())
+			# return blogimporter.main(data)
 	else:
 		return render_template('tn_importer.html')
 
 if __name__ == '__main__':
-    # app.run(debug = True) # Change this to just app.run() in the production version.
-    app.run()
+    app.run(debug = True) # Change this to just app.run() in the production version.
+    # app.run()
---
+++ tnimporter.py
@@ -0,0 +1,54 @@
+import signpostlib
+
+def getLatestTechNewsLink():
+	return signpostlib.makeAPIQuery(titles='Tech/News/Latest', action='query', prop='links', project='meta')[0]['title']
+
+def getLatestTechNewsBody():
+	data = signpostlib.getPageWikicode(getLatestTechNewsLink(), language='meta', project='wikimedia')
+	data = data[data.index('<section begin="tech-newsletter-content"/>'):data.index('<section end="tech-newsletter-content"/>')]
+	return data
+
+def removeDelimitedString(string, front_delimiter, back_delimiter):
+	return string[:string.index(front_delimiter)] + string[string.index(back_delimiter, string.index(front_delimiter)) + len(back_delimiter):]
+
+def main():
+	# Fetch the report body.
+	report_content = getLatestTechNewsBody()
+
+	# Remove meta-only strings from the content.
+	report_content = report_content.replace('<translate>', '')
+	report_content = report_content.replace('</translate>', '')
+	while '<tvar' in report_content:
+		report_content = removeDelimitedString(report_content, '<tvar', '>')
+	while '</>' in report_content:
+		report_content = removeDelimitedString(report_content, '</', '>')
+	while '<!--T' in report_content:
+		report_content = removeDelimitedString(report_content, '<!--', '-->')
+
+	# Fix the resultant formatting errors.
+	report_content = report_content.replace('* \n', '* ')
+	report_content = report_content.replace('*\n', '* ')
+	report_content = report_content.replace('\'\'\'\n\n', '\'\'\'')
+	report_content = report_content.replace('\'\'\'\n', '\'\'\'')
+	report_content = report_content.replace('\'\'\'*', '\'\'\'\n*')
+
+	# Create and format the content string.
+	content = '''{{Signpost draft}}<noinclude>{{Wikipedia:Signpost/Template:Signpost-header|||}}</noinclude>
+
+	{{Wikipedia:Signpost/Template:Signpost-article-start|{{{1|Tech news in brief}}}|By [[meta:Tech/Ambassadors|Wikimedia tech ambassadors]]|'''
+	content += signpostlib.getNextSignpostPublicationDate().strftime('%Y-%m-%d') + '''}}'''
+	content += '\n{{Wikipedia:Wikipedia Signpost/Templates/Tech news}}'
+	content += report_content
+	content += '''\n<noinclude>{{Wikipedia:Signpost/Template:Signpost-article-comments-end||'''
+	content += signpostlib.getPreviousSignpostPublicationDate().strftime('%Y-%m-%d')
+	content += '|'
+	content += '''}}</noinclude>'''
+
+	return content
+
+if __name__ == '__main__':
+	content = main()
+	# Publish.
+	post_point = signpostlib.getNextSignpostPublicationString() + '/Technology report'
+	signpostlib.saveContentToPage(content, post_point, 'Importing Tech News content via the [https://github.com/ResidentMario/TN_Importer TN_Importer] script.')
+	print("Done.")
