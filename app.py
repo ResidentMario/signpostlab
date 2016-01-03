33d6b6f5c3fc6a4af9a14306d28822f78a8e0993
ResidentMario <aleksey.bilogur@gmail.com>
2016-01-02 23:28:13 -0500

    Create image logic.

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import blogimporter
 import requests
 from bs4 import BeautifulSoup
 app = Flask(__name__)
+
+
+def fetch(url):
+    if "blog.wikimedia.org" not in url:
+        raise RuntimeError("The URL" + url + "does not reference the Wikimedia Blog.")
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise RuntimeError("The blog post located at URL " + url + "could not be resolved.")
+    post = r.text[r.text.index('<div class="entry">') + len('<div class="entry">'):]
+    post = post[:post.index('<div class="socials">')]
+    post = post[:post.rindex('</div>')]
+    post = post.replace("	", "")
+    post = requests.post('http://rest.wikimedia.org/en.wikipedia.org/v1/transform/html/to/wikitext',
+                         data={'html': post}).text
+    post = BeautifulSoup(post, "html.parser")
+    post = post.get_text()
+    linefeed = post.split("\n")
+    for line_num in range(0, len(linefeed)):
+        # Remove blog-hosted images and their captions.
+        if "wikimediablog.files.wordpress" in linefeed[line_num]:
+            post = post.replace(linefeed[line_num], "")
+            if linefeed[line_num + 1][:2] == "''":
+                post = post.replace(linefeed[line_num + 1], "")
+            elif linefeed[line_num + 2][:2] == "''":
+                post = post.replace(linefeed[line_num + 1], "")
+        # But convert Commons ones.
+        elif "https://upload.wikimedia.org" in linefeed[line_num]:
+            img_str = linefeed[line_num][linefeed[line_num].rfind("/"):]
+            img_str_copy = img_str[:]
+            # This gets us to 682px-Noor_Mehal_-_WIKI.jpg, still need to remove the filesize.
+            # Because this operation fails in certain edge cases a src is provided for manual corrections
+            img_str = img_str[img_str.find("-") + 1:]
+            # Fix a certain common error.
+            img_str = img_str.replace("]", "")
+            # If a caption exists it'll be punched down onto the next line with ''This formatting.'' Retrieve that.
+            caption = ""
+            if linefeed[line_num + 1][:2] == "''":
+                caption = linefeed[line_num + 1].replace("''", "")
+                post = post.replace(linefeed[line_num + 1], "")
+            elif linefeed[line_num + 2][:2] == "''":
+                caption = linefeed[line_num + 2].replace("''", "")
+                post = post.replace(linefeed[line_num + 2], "")
+            img_str = "{{Signpost inline image|image=File:%s|caption=%s}}\n<!-- %s -->" % (img_str, caption,
+                                                                                           img_str_copy)
+            print(img_str)
+            post = post.replace(linefeed[line_num], img_str)
+    print(post)
+    return post
 
 
 @app.route('/')
@@ -18,24 +65,7 @@ def blog():
         return render_template('blog_importer.html')
     else:
         url = request.form['blog_post_url']
-        # noinspection PyUnresolvedReferences
-        if '//blog.wikimedia.org/' not in url:
-            return render_template('blog_importer.html', code_returned="Failed: please enter a valid URL.")
-        else:
-            if "blog.wikimedia.org" not in url:
-                raise RuntimeError("The URL" + url + "does not reference the Wikimedia Blog.")
-            r = requests.get(url)
-            if r.status_code != 200:
-                raise RuntimeError("The blog post located at URL " + url + "could not be resolved.")
-            post = r.text[r.text.index('<div class="entry">') + len('<div class="entry">'):]
-            post = post[:post.index('<div class="socials">')]
-            post = post[:post.rindex('</div>')]
-            post = post.replace("	", "")
-            post = requests.post('http://rest.wikimedia.org/en.wikipedia.org/v1/transform/html/to/wikitext',
-                                data={'html': post}).text
-            post = BeautifulSoup(post, "html.parser")
-            post = post.get_text()
-            return render_template('blog_importer.html', code_returned=post)
+        return render_template('blog_importer.html', code_returned=fetch(url))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- blogimporter.py
+++ blogimporter.py
@@ -23,14 +23,41 @@ def fetch(url):
     post = r.text[r.text.index('<div class="entry">') + len('<div class="entry">'):]
     post = post[:post.index('<div class="socials">')]
     post = post[:post.rindex('</div>')]
-    # post = post.replace("<p>", "")
-    # post = post.replace("</p>", "NEWLINEPLACEHOLDER")
     post = post.replace("	", "")
     post = requests.post('http://rest.wikimedia.org/en.wikipedia.org/v1/transform/html/to/wikitext',
-                        data={'html': post}).text
+                         data={'html': post}).text
     post = BeautifulSoup(post, "html.parser")
     post = post.get_text()
-    # post = post.replace("NEWLINEPLACEHOLDER",  "\n")
+    linefeed = post.split("\n")
+    for line_num in range(0, len(linefeed)):
+        # Remove blog-hosted images and their captions.
+        if "wikimediablog.files.wordpress" in linefeed[line_num]:
+            post = post.replace(linefeed[line_num], "")
+            if linefeed[line_num + 1][:2] == "''":
+                post = post.replace(linefeed[line_num + 1], "")
+            elif linefeed[line_num + 2][:2] == "''":
+                post = post.replace(linefeed[line_num + 1], "")
+        # But convert Commons ones.
+        elif "https://upload.wikimedia.org" in linefeed[line_num]:
+            img_str = linefeed[line_num][linefeed[line_num].rfind("/"):]
+            img_str_copy = img_str[:]
+            # This gets us to 682px-Noor_Mehal_-_WIKI.jpg, still need to remove the filesize.
+            # Because this operation fails in certain edge cases a src is provided for manual corrections
+            img_str = img_str[img_str.find("-") + 1:]
+            # Fix a certain common error.
+            img_str = img_str.replace("]", "")
+            # If a caption exists it'll be punched down onto the next line with ''This formatting.'' Retrieve that.
+            caption = ""
+            if linefeed[line_num + 1][:2] == "''":
+                caption = linefeed[line_num + 1].replace("''", "")
+                post = post.replace(linefeed[line_num + 1], "")
+            elif linefeed[line_num + 2][:2] == "''":
+                caption = linefeed[line_num + 2].replace("''", "")
+                post = post.replace(linefeed[line_num + 2], "")
+            img_str = "{{Signpost inline image|image=File:%s|caption=%s}}\n<!-- %s -->" % (img_str, caption,
+                                                                                           img_str_copy)
+            print(img_str)
+            post = post.replace(linefeed[line_num], img_str)
     print(post)
     return post
 
@@ -38,73 +65,3 @@ def fetch(url):
 def save(text, filename='output.txt'):
     with open(filename, mode='w') as fn:
         fn.write(text)
-
-
-# def main(page, target=signpostlib.get_next_signpost_issue() + '/Blog'):
-#     # Check to make sure that the --page argument is filled in with valid input, and return an error if it is not.
-#     if not page or 'blog.wikimedia.org' not in page:
-#         raise IOError(
-#                 "This script requires a valid --page parameter. Otherwise the script doesn\'t know what Blog content "
-#                 "to intake! For example try: python Blog_Importer.py --page "
-#                 "\'https://blog.wikimedia.org/2015/07/16/third-transparency-report-released/\'")
-#
-#     # Check if a target is provided. If it is, make sure it is valid; if it isn't have the script fetch it.
-#     if 'User:Resident Mario/' not in target and 'Wikipedia:Wikipedia Signpost/' not in target:
-#         raise IOError(
-#                 'A target page was provided but did not conform to legal targets for this script. Please direct your target at a subpage of User:Resident Mario or of Wikipedia:Wikipedia Signpost. To just set it to the next Signpost issue, don\'t provide this argument at all.')
-#
-#     # Fetch and store the contents of the blog post.
-#     post = requests.get(page).text
-#
-#     # Core the data to the post itself.
-#     post = post[post.index('<div class="entry">') + len('<div class="entry">'):]
-#     post = post[:post.index('<div class="socials">')]
-#     post = post[:post.rfind('</div>')]
-#
-#     # Use the RESTBase API to convert the blog's HTML to native wikicode.
-#     post = signpostlib.htmlToWikitext(post)
-#
-#     # Package the post for inclusion in the Signpost.
-#     post = '''<noinclude>{{Signpost draft}}
-# 	{{Wikipedia:Signpost/Template:Signpost-header|||}}</noinclude>
-#
-# 	<div style="padding-left:50px; padding-right:50px;">
-#
-# 	{{Wikipedia:Signpost/Template:Signpost-article-start|{{{1|Your title}}}|By ?| {{subst:#time:j F Y|{{subst:Wikipedia:Wikipedia Signpost/Issue|4}}}}}}
-#
-# 	</div>
-#
-# 	{{Wikipedia:Wikipedia Signpost/Templates/WM Blog}}
-#
-# 	<div style="width:46em; line-height:1.6em; font-size:1em; font-family:Helvetica Neue, Helvetica, Arial, sans-serif; padding-left:5em;" class="plainlinks">''' + post + '''</div>
-#
-# 	<noinclude>{{Wikipedia:Signpost/Template:Signpost-article-comments-end||2015-04-22|2015-05-06}}
-# 	</noinclude>'''
-#     return post
-#
-#
-# # The following instructions are only executed with this script is called from the command line.
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-p", "--page",
-#                         help='The webpage from which this script will attempt to intake content. Must be a page in '
-#                              'the Wikimedia Blog domain. This is a required argument.')
-#     parser.add_argument("-t", "--target",
-#                         help='The target page to which the output of this script will be written. Must be a subpage '
-#                              'of the Wikipedia Signpost or in the userspace this script\'s writer, Resident Mario.')
-#     args = parser.parse_args()
-#
-#     # Check to make sure that the --page argument is filled in with valid input, and return an error if it is not.
-#     if not args.page or 'blog.wikimedia.org' not in args.page:
-#         raise IOError(
-#                 'This script requires a valid --page parameter. Otherwise the script doesn\'t know what Blog content to intake! For example try: python Blog_Importer.py --page \'https://blog.wikimedia.org/2015/07/16/third-transparency-report-released/\'')
-#
-#     # Check if a target is provided. If it is, make sure it is valid; if it isn't have the script fetch it.
-#     if not args.target:
-#         args.target = signpostlib.get_next_signpost_issue() + '/Blog'
-#     elif 'User:Resident Mario/' not in args.target and 'Wikipedia:Wikipedia Signpost/' not in args.target:
-#         raise IOError(
-#                 'A target page was provided but did not conform to legal targets for this script. Please direct your target at a subpage of User:Resident Mario or of Wikipedia:Wikipedia Signpost. To just set it to the next Signpost issue, don\'t provide this argument at all.')
-#     post = main(args.page, args.target)
-#     signpostlib.saveContentToPage(post, args.target,
-#                                   'Importing basic Blog repost via the [https://github.com/ResidentMario/Blog_Importer Blog_Importer] script.')
