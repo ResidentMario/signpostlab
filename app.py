a7b5b4f7f91befc54c68700dbcb7fe41bf137dfe
ResidentMario <aleksey.bilogur@gmail.com>
2016-01-02 22:22:12 -0500

    Attempt to fix.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import blogimporter
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
@@ -20,8 +22,20 @@ def blog():
         if '//blog.wikimedia.org/' not in url:
             return render_template('blog_importer.html', code_returned="Failed: please enter a valid URL.")
         else:
-            ret = blogimporter.fetch(url)
-            return render_template('blog_importer.html', code_returned=ret)
+            if "blog.wikimedia.org" not in url:
+                raise RuntimeError("The URL" + url + "does not reference the Wikimedia Blog.")
+            r = requests.get(url)
+            if r.status_code != 200:
+                raise RuntimeError("The blog post located at URL " + url + "could not be resolved.")
+            post = r.text[r.text.index('<div class="entry">') + len('<div class="entry">'):]
+            post = post[:post.index('<div class="socials">')]
+            post = post[:post.rindex('</div>')]
+            post = post.replace("	", "")
+            post = requests.post('http://rest.wikimedia.org/en.wikipedia.org/v1/transform/html/to/wikitext',
+                                data={'html': post}).text
+            post = BeautifulSoup(post, "html.parser")
+            post = post.get_text()
+            return render_template('blog_importer.html', code_returned=post)
 
 if __name__ == '__main__':
     app.run(debug=True)
